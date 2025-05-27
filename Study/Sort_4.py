6d8848bc16b4d31abc7948affe7563fac8aca5bd
kws0315 <82193629+kws0315@users.noreply.github.com>
2025-05-27 16:34:15 +0900

    Update Sort_4.py

--- Study/Sort_4.py
+++ Study/Sort_4.py
@@ -4,6 +4,7 @@ def insertionSort(ary):
         for cur in range(end, 0, -1):
             if ary[cur-1] > ary[cur]:
                 ary[cur-1], ary[cur] = ary[cur], ary[cur-1] #서로의 값을 바꾸기 (tmp없이 가능)
+    return ary
 
 ## 전역 변수 ##
 dataAry = [188, 168, 177, 120, 88, 130, 77, 54, 230, 229]
